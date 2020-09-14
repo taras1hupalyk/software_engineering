69a810c99168f3918a3e5c97954cddbaeaf5f04e
taras1hupalyk <t.hupalyk@gmail.com>
2020-09-14 18:34:52 +0300

    Inital commit

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            FibonacciCalculate(length);
+        }
+
+        public static void FibonacciCalculate(int len)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
